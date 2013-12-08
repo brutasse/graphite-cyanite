af70bb5527cc8235684eab1ff6a6601babc4f532
Bruno Renié <brutasse@gmail.com>
2013-12-08 16:14:16 +0100

    Initial version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='cyanite',
-    version='0.1',
+    version='0.1.0',
     url='https://github.com/brutasse/graphite-cyanite',
     license='BSD',
     author=u'Bruno Renié',
