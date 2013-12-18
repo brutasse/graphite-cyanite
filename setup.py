9e9f58844a21f28f8052c943901e9564d4fe3d0a
Bruno Renié <brutasse@gmail.com>
2013-12-18 22:55:25 +0100

    Fix setup.py

--- setup.py
+++ setup.py
@@ -10,13 +10,14 @@ setup(
     author_email='bruno@renie.fr',
     description=('A plugin for using graphite-web with the cassandra-based '
                  'Cyanite storage backend'),
+    long_description=open('README.rst').read(),
     py_modules=('cyanite',),
     zip_safe=False,
     include_package_data=True,
     platforms='any',
     classifiers=(
         'Intended Audience :: Developers',
-        'Intended Audience :: System Administrators'
+        'Intended Audience :: System Administrators',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
